bc7d5086dc930e626ffe9e3cedfc59febae03595
Frank Albanese <albanesefc9@gmail.com>
2022-09-19 12:40:51 -0400

    Login page layout complete

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Login } from './Login';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    
+    <Login />
   </React.StrictMode>
 );
 
