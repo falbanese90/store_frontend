f76997811feeecabdd98c25ed1287919a75363ad
Frank Elia <frankelia@franks-air.myfiosgateway.com>
2022-09-20 09:26:50 -0400

    Replaced Email with username

--- src/Login.js
+++ src/Login.js
@@ -7,24 +7,25 @@ export class Login extends React.Component {
     constructor(props) {
         super(props);
         this.state = { username: '', password: '' };
+        this.login = this.login.bind(this);
+    }
+    login(e) {
+        alert(`Username: ${this.state.username}\nPassword: ${this.state.password}`);
     }
     render() {
         return (
             <div className='Login'>
-                <Form>
-                    <Form.Group classname='mb-3' controlId='formBasicEmail'>
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control type='email' placeholder='Enter Email' />
-                        <Form.Text className='text-muted'>
-                            We'll never share your email with anyone else.
-                        </Form.Text>
+                <Form onSubmit={this.login}>
+                    <Form.Group className='mb-3'>
+                        <Form.Label>Username</Form.Label>
+                        <Form.Control type='username' placeholder='Enter Username' onChange={e => this.setState({ username: e.target.value })} />
                     </Form.Group>
 
                     <Form.Group className='mb-3' controlId='formBasicPassword'>
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type='password' placeholder='Password' />
+                        <Form.Control type='password' placeholder='Password' onChange={e => this.setState({ password: e.target.value })} />
                     </Form.Group>
-                    <Form.Group classname='mb-3' controlId='formBasicCheckbox'>
+                    <Form.Group className='mb-3' controlId='formBasicCheckbox'>
                         <Form.Check type='checkbox' label='Check me out' />
                     </Form.Group>
                     <Button variant='primary' type='submit'>
